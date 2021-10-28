ba970951c65e80af93668b9d6a1bcd4840070ed0
ilias asrarfi <aae.ilyass1890@gmail.com>
2021-10-28 22:52:49 +0100

    add button for go to top

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,29 @@ window.addEventListener("DOMContentLoaded", (event) => {
     elements: "#portfolio a.portfolio-box",
   });
 });
+
+
+// button to go up
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//Get the button end
+
